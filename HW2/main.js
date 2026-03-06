947e7bcdfb11e0a29525a60d4508dd49e248dee1
Nick Thedens <nthedens@uiowa.edu>
2026-03-05 20:02:27 -0600

    created js file code

---
+++ HW2/main.js
@@ -0,0 +1,55 @@
+(function () {
+   
+   
+   //API_URL
+   const API_URL='https://api.datausa.io/tesseract/data.jsonrecords?cube=acs_yg_total_population_5&measures=Population&drilldowns=Year';
+   const container = document.getElementById('table-container');
+   async function fetchPopulation() {
+    try {
+        const response =  await fetch(API_URL);
+        //check response
+        if (!response.ok) {
+            throw new Error('HTTP error! Status: ${response.status}');
+        }
+
+        const jsonPopData= await response.json();
+        const PopDataArray= jsonPopData.data;
+
+        //Sort data by year
+        PopDataArray.sort((a,b)=>b.Year-a.Year);
+        
+        // Build the table 
+        
+        let tableHTML = `
+            <table class="custom-table">
+                <thead>
+                     <tr>
+                         <th>Year</th>
+                        <th>Population</th>
+                    </tr>
+                </thead>
+                <tbody>
+        `;
+    for (let i =0; i<PopDataArray.length;i++){
+        let yearVal = PopDataArray[i].Year;
+        let populationVal = PopDataArray[i].Population.toLocaleString();
+        tableHTML += `
+        <tr>
+            <td>${yearVal}</td>
+            <td>${populationVal}</td>
+        </tr>
+        `;
+    }
+    tableHTML += `
+            </tbody>
+        </table>
+        `;
+    container.innerHTML= tableHTML;
+    } catch (error) {
+        console.error(error);
+        container.innerHTML ='<p style ="color:red">Data failed to load.</p>';
+    }
+   }
+   fetchPopulation();
+}
+)();

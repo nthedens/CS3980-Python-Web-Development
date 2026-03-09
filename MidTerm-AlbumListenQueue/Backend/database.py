56cd5c0e814ccb77c0ecfba65cdea515569220f0
Nick Thedens <nthedens@uiowa.edu>
2026-03-08 23:29:03 -0500

    capitalized intial database strings

--- MidTerm-AlbumListenQueue/Backend/database.py
+++ MidTerm-AlbumListenQueue/Backend/database.py
@@ -2,10 +2,10 @@ from typing import List
 from Backend.model import Album,ListenedAlbum
 
 album_list: List[Album] = [
-    Album(id=1, title="Amnesiac",artist="Radiohead",year=2001,listen_format="digital", priority=True)
+    Album(id=1, title="Amnesiac",artist="Radiohead",year=2001,listen_format="Digital", priority=True)
 
 ]
 
 listened_album_list: List[ListenedAlbum] = [
-    ListenedAlbum(id=1, title="Dreams", artist="Elephant Gym", year=2022,listen_format="cd", rating=7.8)
+    ListenedAlbum(id=1, title="Dreams", artist="Elephant Gym", year=2022,listen_format="CD", rating=7.8)
 ]
